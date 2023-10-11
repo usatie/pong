575024d7bb9ba05228cf231a9bc1bb9b40e4c64f
Shun Usami <usatie@gmail.com>
2023-10-11 12:12:44 +0900

    Add sample data to Task
    - `npx ts-node src/seeding/todo.ts`
    - `docker exec -it todo-postgres /bin/psql -h localhost -p 5432 -U postgres -d postgres`
    - `SELECT * FROM "Task";`

---
+++ playground/my-nest-sample/src/seeding/todo.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+	const tasks = [
+		"Buy milk",
+		"Write a blog post",
+		"Go for a run",
+	]
+
+	await Promise.all(
+		tasks.map((task) => {
+			return prisma.task.create({
+				data: {
+					title: task
+				}
+			})
+		})
+	)
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async (e) => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
