61dc4e31cc89e2a934ea867a0b2018fc86b75ae9
Takumi Hara <takumihara1226@gmail.com>
2023-10-25 17:21:07 +0900

    fetch data from backend without cache

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,10 +1,21 @@
 async function getUsers() {
-	const res = await fetch("http://backend:3000/api/user");
-	const users = await res.json();
-	return users;
+  const res = await fetch("http://backend:3000/api/user", {
+    cache: "no-cache",
+  });
+  const users = await res.json();
+  return users;
 }
 
 export default async function User() {
-	const users = await getUsers();
-	return (<div>{ users.map((user) => <li>{user.name}({user.id}) : {user.email}</li>) }</div>);
+  const users = await getUsers();
+  console.log(users);
+  return (
+    <div>
+      {users.map((user) => (
+        <li>
+          {user.name}({user.id}) : {user.email}
+        </li>
+      ))}
+    </div>
+  );
 }
