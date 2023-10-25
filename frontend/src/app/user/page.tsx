20fba2a9a0f5ac928ecacc93273d3b70a9ef05d7
Takumi Hara <takumihara1226@gmail.com>
2023-10-25 18:26:38 +0900

    Create user list page

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,3 +1,16 @@
+import Nav from "@/components/Nav";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
 async function getUsers() {
   const res = await fetch("http://backend:3000/api/user", {
     cache: "no-cache",
@@ -8,14 +21,34 @@ async function getUsers() {
 
 export default async function User() {
   const users = await getUsers();
-  console.log(users);
   return (
-    <div>
-      {users.map((user) => (
-        <li>
-          {user.name}({user.id}) : {user.email}
-        </li>
-      ))}
-    </div>
+    <main className="flex flex-col gap-8 p-24">
+      <Nav />
+      <div className="flex flex-col gap-3">
+        {users.map((user, index) => (
+          <Card className="w-[350px]" key={index}>
+            <CardHeader></CardHeader>
+            <CardContent>
+              <form>
+                <div className="grid w-full items-center gap-4">
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="name">Name</Label>
+                    <Input id="name" value={user.name} />
+                  </div>
+                  <div className="flex flex-col space-y-1.5">
+                    <Label htmlFor="email">Email</Label>
+                    <Input id="email" value={user.email} />
+                  </div>
+                </div>
+              </form>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <Button variant="outline">Delete</Button>
+              <Button>Update</Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
   );
 }
