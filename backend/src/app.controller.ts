b791ad56e1c33c0e2981919f6dfda3b80c20ab2d
Takumi Hara <takumihara1226@gmail.com>
2023-10-25 12:54:48 +0900

    Add DATABASE_URL

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,10 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import * as pgPromise from 'pg-promise';
-
-const pgp = pgPromise({});
-// postgres://user:password@host:port/database
-const db = pgp(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@db:5432/${process.env.POSTGRES_DB}`);
 
 @Controller()
 export class AppController {
