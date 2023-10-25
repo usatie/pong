e9ed84ddb106497a94efc192343f7949eb4d84cd
Shun Usami <usatie@gmail.com>
2023-10-25 15:50:42 +0900

    Add user list page

--- frontend/src/app/user/page.tsx
+++ frontend/src/app/user/page.tsx
@@ -1,3 +1,10 @@
-export default function User() {
-	return <div>User</div>;
+async function getUsers() {
+	const res = await fetch("http://backend:3000/api/user");
+	const users = await res.json();
+	return users;
+}
+
+export default async function User() {
+	const users = await getUsers();
+	return (<div>{ users.map((user) => <li>{user.name}({user.id}) : {user.email}</li>) }</div>);
 }
