4fcdd692e98dbe0f51c69f2ffa221522f54a8d7a
Shun Usami <usatie@gmail.com>
2023-11-07 13:06:50 +0900

    [backend] Implement auth.module

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaModule } from 'src/prisma/prisma.module';
+
+export const jwtConstants = {
+	secret: process.env.JWT_SECRET,
+};
 
 @Module({
+  imports: [
+	  PrismaModule,
+	  PassportModule,
+	  JwtModule.register({
+		  secret: jwtConstants.secret,
+		  signOptions: { expiresIn: '30m' }, // 30 minutes
+	  }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
