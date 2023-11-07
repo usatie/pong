24caa009396c12030f2f342d332d00e0e2cc6ca2
Shun Usami <usatie@gmail.com>
2023-11-07 10:51:42 +0900

    [backend] Add auth module and passport to backend
    - `docker compose exec backend yarn run nest generate resource`
    - `npm install --save @nestjs/passport passport @nestjs/jwt passport-jwt`
    - `npm install --save-dev @types/passport-jwt`

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [UserModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
