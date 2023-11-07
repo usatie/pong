89f235e5233505739bf54b4c6494b91a978a2313
Shun Usami <usatie@gmail.com>
2023-11-07 13:14:12 +0900

    [backend] Implement auth.service
    - Create a new file `backend/src/auth/dto/login.dto.ts`
    - Create a new file `backend/src/auth/entity/auth.entity.ts`

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { 
+	Injectable,
+	NotFoundException,
+	UnauthorizedException,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { AuthEntity } from './entity/auth.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+	async login(email: string, password: string): Promise<AuthEntity> {
+		const user = await this.prisma.user.findUnique({ where: { email } });
+
+		if (!user) {
+			throw new NotFoundException(`No user found for email: ${email}`);
+		}
+
+		// TODO: Use bcrypt to compare the password
+		const isPasswordValid = user.password === password;
+
+		if (!isPasswordValid) {
+			throw new UnauthorizedException('Invalid password');
+		}
+
+		return {
+			accessToken: this.jwtService.sign({ userId: user.id }),
+		}
+	}
+}
