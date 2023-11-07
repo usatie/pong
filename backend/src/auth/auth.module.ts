873045e344d03914de1748bea5e0b8d2fb2998ca
Shun Usami <usatie@gmail.com>
2023-11-07 13:54:01 +0900

    [backend] Add the new JwtStrategy as a provider
    - Export the UserService from the UserModule

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserModule } from 'src/user/user.module';
+import { JwtStrategy } from './jwt.strategy';
 
 export const jwtConstants = {
 	secret: process.env.JWT_SECRET,
@@ -17,8 +19,9 @@ export const jwtConstants = {
 		  secret: jwtConstants.secret,
 		  signOptions: { expiresIn: '30m' }, // 30 minutes
 	  }),
+	  UserModule,
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
