2e0a5621b9a3b3d8edbb2cd1aabf21e212a1490d
lim <lbooks424@gmail.com>
2023-11-08 16:47:37 +0900

    [WIP] create TEST

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -21,4 +21,56 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
+
+  it('/user (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/user')
+      .expect(200);
+  });
+
+  it('/user/1 (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/user/1')
+      .expect(401);
+  });
+
+  let userId;
+  it('/user (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/user')
+      .send({
+        email: "test@email.com",
+        name: "test_user",
+        password: "password-test"
+      })
+      .expect(201)
+      .then(response => {
+        userId = response.body.userId;
+      });
+  });
+
+  it('/auth/login (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        email: "test@email.com",
+        password: "password-test"
+      })
+      .expect(201);
+  });
+
+  it('/user/:id (DELETE)', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        email: "test@email.com",
+        password: "password-test"
+      })
+      .expect(201);
+    const { accessToken } = res.body;
+    return request(app.getHttpServer())
+      .delete(`/user/${userId}`)
+      .set("Authorization", `Bearer ${accessToken}`)
+      .expect(204);
+  });
 });
