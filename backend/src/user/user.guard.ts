d4e5f38b3fe769660ac427e109951e6ccccd6d21
dorakueYaritai <kotto555555@gmail.com>
2023-11-08 13:58:51 +0900

    [backend] Implement UserGuard and add it to UserController

--- backend/src/user/user.guard.ts
+++ backend/src/user/user.guard.ts
@@ -1,11 +1,19 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { UserService } from './user.service';
 
 @Injectable()
 export class UserGuard implements CanActivate {
+	constructor(private readonly userService: UserService) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+	const req = context.switchToHttp().getRequest();
+	const { params, user } = req;
+	if (params?.id == null) {
+		return true;
+	}
+	return user.id === Number(params.id);
   }
 }
