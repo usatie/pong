e0899432a0c583bcebee489df07299a3ab9d5f39
Shun Usami <usatie@users.noreply.github.com>
2023-11-09 03:45:23 +0900

    Add code format check and linter check to CI (#28)
    * [ci] Add code format check to CI
    * [backend] Format code with prettier
    * [backend] Fix lint errors (defined but never used)

--- backend/src/user/user.guard.ts
+++ backend/src/user/user.guard.ts
@@ -2,15 +2,14 @@ import { CanActivate, ExecutionContext } from '@nestjs/common';
 import { Observable } from 'rxjs';
 
 export class UserGuard implements CanActivate {
-
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-	const req = context.switchToHttp().getRequest();
-	const { params, user } = req;
-	if (params?.id == null) {
-		return true;
-	}
-	return user.id === Number(params.id);
+    const req = context.switchToHttp().getRequest();
+    const { params, user } = req;
+    if (params?.id == null) {
+      return true;
+    }
+    return user.id === Number(params.id);
   }
 }
