32634e389145057d01f4c44a7430340bf99d1625
Shun Usami <usatie@users.noreply.github.com>
2023-11-09 11:08:04 +0900

    Enhance backend e2e tests for user endpoints (#34)
    * [backend] Refactor e2e test using testUser object
    * [backend] Add global pipes, interceptors and filters to e2e tests
    * [backend] Add invalid authentication tests
    * [backend] Add invalid sign up tests
    * [backend] Add sign up, log in and delete user scenario tests
    * [Makefile] Add lint and check targets

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -2,9 +2,18 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { HttpAdapterHost, Reflector } from '@nestjs/core';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const testUser = {
+    name: 'test_user',
+    email: 'test@example.com',
+    password: 'password-test',
+  };
+  const testUserLogin = { email: testUser.email, password: testUser.password };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,51 +21,191 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // enable validation
+    app.useGlobalInterceptors(
+      new ClassSerializerInterceptor(app.get(Reflector)),
+    ); // enable serialization
+    const { httpAdapter } = app.get(HttpAdapterHost);
+    app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/ (GET)', () => {
+    it('should return "Hello World!"', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('Hello World!');
+    });
   });
 
-  it('/user (GET)', () => {
-    return request(app.getHttpServer()).get('/user').expect(200);
-  });
+  describe('/user', () => {
+    const testUserResponse = (user) => {
+      expect(user).toHaveProperty('id');
+      expect(user).toHaveProperty('name');
+      expect(user).toHaveProperty('email');
+      expect(user).not.toHaveProperty('password');
+    };
+    describe('Without authentiation', () => {
+      it('GET /user should return users list', async () => {
+        const res = await request(app.getHttpServer()).get('/user').expect(200);
+        const users = res.body;
+        expect(users).toBeInstanceOf(Array);
+        expect(users.length).toBeGreaterThan(0);
+        users.forEach(testUserResponse);
+      });
 
-  it('/user/1 (GET)', () => {
-    return request(app.getHttpServer()).get('/user/1').expect(401);
-  });
+      it('GET /user/:id should return 401 Unauthorized', () => {
+        return request(app.getHttpServer()).get('/user/1').expect(401);
+      });
 
-  it('/user (POST); /auth/login (POST) /user/:id (DELETE)', async () => {
-    const userEmail = 'test@email.com';
-    const userPassword = 'password-test';
-    const userName = 'test_user';
+      it('PATCH /user/:id should return 401 Unauthorized', () => {
+        return request(app.getHttpServer()).patch('/user/1').expect(401);
+      });
 
-    const id = await request(app.getHttpServer())
-      .post('/user')
-      .send({
-        email: userEmail,
-        name: userName,
-        password: userPassword,
-      })
-      .expect(201)
-      .then((res) => res.body.id);
+      it('DELETE /user/:id should return 401 Unauthorized', () => {
+        return request(app.getHttpServer()).delete('/user/1').expect(401);
+      });
+    });
 
-    const accessToken = await request(app.getHttpServer())
-      .post('/auth/login')
-      .send({
-        email: userEmail,
-        password: userPassword,
-      })
-      .expect(201)
-      .then((res) => res.body.accessToken);
+    describe('Invalid Sign up', () => {
+      it('POST /user with invalid email should return 400 Bad Request', () => {
+        const invalidUser = { ...testUser, email: 'invalid' };
+        return request(app.getHttpServer())
+          .post('/user')
+          .send(invalidUser)
+          .expect(400);
+      });
 
-    return request(app.getHttpServer())
-      .delete(`/user/${id}`)
-      .set('Authorization', `Bearer ${accessToken}`)
-      .expect(204);
+      it('POST /user with too short name should return 400 Bad Request', () => {
+        const invalidUser = { ...testUser, name: 'a' };
+        return request(app.getHttpServer())
+          .post('/user')
+          .send(invalidUser)
+          .expect(400);
+      });
+
+      it('POST /user with too short password should return 400 Bad Request', () => {
+        const invalidUser = { ...testUser, password: 'short' };
+        return request(app.getHttpServer())
+          .post('/user')
+          .send(invalidUser)
+          .expect(400);
+      });
+    });
+
+    describe('Invalid authentication', () => {
+      it('POST /auth/login with invalid email should return 400 Bad Request', () => {
+        const invalidLogin = { ...testUserLogin, email: 'invalid' };
+        return request(app.getHttpServer())
+          .post('/auth/login')
+          .send(invalidLogin)
+          .expect(400);
+      });
+
+      it('POST /auth/login with email not registered should return 404 Not Found', () => {
+        const invalidLogin = {
+          ...testUserLogin,
+          email: 'nosuchuser@example.com',
+        };
+        return request(app.getHttpServer())
+          .post('/auth/login')
+          .send(invalidLogin)
+          .expect(404);
+      });
+
+      it('POST /auth/login with invalid password should return 401 Unauthorized', () => {
+        const invalidLogin = {
+          email: 'susami@example.com',
+          password: 'invalid',
+        };
+        return request(app.getHttpServer())
+          .post('/auth/login')
+          .send(invalidLogin)
+          .expect(401);
+      });
+    });
+
+    describe('Sign up, Log in and Delete', () => {
+      it('[POST /user] => [POST /auth/login] => [DELETE /user/:id]', async () => {
+        const id = await request(app.getHttpServer())
+          .post('/user')
+          .send(testUser)
+          .expect(201)
+          .then((res) => {
+            const user = res.body;
+            testUserResponse(res.body);
+            return user.id;
+          });
+
+        const accessToken = await request(app.getHttpServer())
+          .post('/auth/login')
+          .send(testUserLogin)
+          .expect(201)
+          .then((res) => res.body.accessToken);
+
+        return request(app.getHttpServer())
+          .delete(`/user/${id}`)
+          .set('Authorization', `Bearer ${accessToken}`)
+          .expect(204)
+          .expect({});
+      });
+    });
+
+    describe('With authentication', () => {
+      let id: number;
+      let accessToken: string;
+
+      beforeAll(async () => {
+        id = await request(app.getHttpServer())
+          .post('/user')
+          .send(testUser)
+          .then((res) => res.body.id);
+
+        accessToken = await request(app.getHttpServer())
+          .post('/auth/login')
+          .send(testUserLogin)
+          .then((res) => res.body.accessToken);
+      });
+
+      afterAll(async () => {
+        await request(app.getHttpServer())
+          .delete(`/user/${id}`)
+          .set('Authorization', `Bearer ${accessToken}`);
+      });
+
+      it('GET /user/:id should return the user', () => {
+        const expectedUser = { ...testUser, id };
+        delete expectedUser.password;
+        return request(app.getHttpServer())
+          .get(`/user/${id}`)
+          .set('Authorization', `Bearer ${accessToken}`)
+          .expect(200)
+          .expect(expectedUser);
+      });
+
+      it('PATCH /user/:id should update the user', () => {
+        const updatedUser = {
+          ...testUser,
+          id,
+          name: 'new_name',
+        };
+        delete updatedUser.password;
+        return request(app.getHttpServer())
+          .patch(`/user/${id}`)
+          .set('Authorization', `Bearer ${accessToken}`)
+          .send(updatedUser)
+          .expect(200)
+          .expect(updatedUser);
+      });
+
+      it('DELETE /user/:id should delete the user', () => {
+        return request(app.getHttpServer())
+          .delete(`/user/${id}`)
+          .set('Authorization', `Bearer ${accessToken}`)
+          .expect(204)
+          .expect({});
+      });
+    });
   });
 });
