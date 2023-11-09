847cc36bad1e0bf9d95b1ba0051ba8374342ccc2
dorakueYaritai <kotto555555@gmail.com>
2023-11-09 17:39:22 +0900

    fix test add prismaservice room controller and service need PrismaService

--- backend/src/room/room.service.spec.ts
+++ backend/src/room/room.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { RoomService } from './room.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('RoomService', () => {
   let service: RoomService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RoomService],
+      providers: [RoomService, PrismaService],
     }).compile();
 
     service = module.get<RoomService>(RoomService);
