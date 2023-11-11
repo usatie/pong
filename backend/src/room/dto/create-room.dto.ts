190db5ff65e24962816239752fddf9908b4b1e11
lim396 <90199432+lim396@users.noreply.github.com>
2023-11-11 13:51:07 +0900

    [WIP] Validation of room CRUD  (#39)
    * [mod] ParseIntPipe to convert room id from string to number in dynamic URL paths

--- backend/src/room/dto/create-room.dto.ts
+++ backend/src/room/dto/create-room.dto.ts
@@ -1,11 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class CreateRoomDto {
   @ApiProperty()
   id: number;
 
   @IsString()
+  @IsNotEmpty()
   @ApiProperty()
   name: string;
 }
