18eb3d73f815079e9b7952efa0e8c8677edaa1f0
lim <lbooks424@gmail.com>
2023-11-15 16:16:12 +0900

    [frontend] GET /user/{id} can now be hit from frontend with authentication

--- frontend/app/user/[id]/page.tsx
+++ frontend/app/user/[id]/page.tsx
@@ -1,8 +1,14 @@
 import UserCard from "@/app/ui/user/card";
+import { cookies } from "next/headers";
 
 async function getUser(id: number) {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get("token");
   const res = await fetch(`${process.env.API_URL}/user/${id}`, {
     cache: "no-cache",
+    headers: {
+      Authorization: "Bearer " + accessToken.value,
+    },
   });
   const user = await res.json();
   return user;
@@ -14,5 +20,6 @@ export default async function FindUser({
   params: { id: number };
 }) {
   const user = await getUser(id);
+  console.log(user);
   return <UserCard user={user} />;
 }
