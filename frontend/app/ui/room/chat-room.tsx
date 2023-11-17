891a00e05234606b8ec2d8382f86431e261aaee2
lim <lbooks424@gmail.com>
2023-11-17 14:06:56 +0900

    [frontend] Experimental ChatRoom page with socket.io(Accessed by room/{id})

---
+++ frontend/app/ui/room/chat-room.tsx
@@ -0,0 +1,33 @@
+import { Card, CardHeader, CardContent, CardFooter, CardDescription, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+export default function ChatHome() {
+  return (
+    <div className="flex items-center justify-center">
+      <Card className="w-[800px] h-[650px] grid grid-rows-[min-content_1fr_min-content]">
+        <CardHeader>
+          <CardTitle>Chat room</CardTitle>
+          <CardDescription>Experimental chat room</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-3">
+          <div className="flex gap-3 text-slate-600 text-sm">
+            <p className="mt-2">
+              example chat: Hello!
+            </p>
+          </div>
+          <div className="flex gap-3 text-slate-600 text-sm">
+            <p className="ml-2">
+              example chat: Hello!
+            </p>
+          </div>
+        </CardContent>
+        <CardFooter className="space-x-2">
+          <Input placeholder="Message..." />
+          <Button type="submit">Send</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  )
+}
+
