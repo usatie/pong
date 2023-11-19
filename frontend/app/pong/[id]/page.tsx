c2fe171e0aecc99c9efaec1bd07e211cfd7c54c0
Takumi Hara <takumihara1226@gmail.com>
2023-11-18 18:50:08 -0800

    Refactor pong (more react like)

--- frontend/app/pong/[id]/page.tsx
+++ frontend/app/pong/[id]/page.tsx
@@ -1,54 +1,56 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import io from "Socket.IO-client";
-import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import { useEffect, useRef, useState } from "react";
 import { PongGame } from "./PongGame";
 import { TARGET_FRAME_MS } from "./const";
 
-const socket = io("https://pong.shunusami.com/");
+// todo
+const socket = io(process.env.NEXT_PUBLIC_WEB_URL as string);
 
 export default function Page() {
-  let game: PongGame;
-  const [fps, setFps] = useState(0);
-  const [speed, setSpeed] = useState(0);
-  const [player1Position, setPlayer1Position] = useState(0);
-  const [player2Position, setPlayer2Position] = useState(0);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  // todo: check if useRef is necessary. Just const is enough as it doesn't re-render?
+  const game = useRef<PongGame>(new PongGame(canvasRef, socket));
 
-  socket.on("connect", () => {
-    console.log(`Connected: ${socket.id}`);
-
-    socket.emit("join");
-  });
-
-  socket.on("start", (data) => {
-    console.log(`Start: ${JSON.stringify(data)}`);
-    game.start(data);
-  });
-
-  socket.on("right", () => {
-    game.player2.clear(game.ctx);
-    game.player2.move_left();
-    game.player2.draw(game.ctx);
-  });
-
-  socket.on("left", () => {
-    game.player2.clear(game.ctx);
-    game.player2.move_right();
-    game.player2.draw(game.ctx);
-  });
-
-  // Key Events
   useEffect(() => {
-    const handleKeyUp = (event) => {
-      game.keypress[event.key] = false;
+    game.current.setup_canvas();
+    game.current.draw_canvas();
+    setInterval(game.current.update, TARGET_FRAME_MS);
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+      game.current.keypress[event.key] = false;
     };
-    const handleKeyDown = (event) => {
-      game.keypress[event.key] = true;
+    const handleKeyDown = (event: KeyboardEvent) => {
+      game.current.keypress[event.key] = true;
     };
 
     document.addEventListener("keydown", handleKeyDown);
 
     document.addEventListener("keyup", handleKeyUp);
+
+    socket.on("connect", () => {
+      console.log(`Connected: ${socket.id}`);
+
+      socket.emit("join");
+    });
+
+    socket.on("start", (data) => {
+      console.log(`Start: ${JSON.stringify(data)}`);
+      game.current.start(data);
+    });
+
+    socket.on("right", () => {
+      game.current.player2.clear(game.current.ctx);
+      game.current.player2.move_left();
+      game.current.player2.draw(game.current.ctx);
+    });
+
+    socket.on("left", () => {
+      game.current.player2.clear(game.current.ctx);
+      game.current.player2.move_right();
+      game.current.player2.draw(game.current.ctx);
+    });
 
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
@@ -57,60 +59,39 @@ export default function Page() {
   }, []);
 
   const start = () => {
-    game.start();
+    game.current.start();
     socket.emit("start", {
-      vx: -game.ball.vx,
-      vy: -game.ball.vy,
+      vx: -game.current.ball.vx,
+      vy: -game.current.ball.vy,
     });
   };
 
-  useEffect(() => {
-    const canvas: HTMLCanvasElement = document.getElementById(
-      "tutorial",
-    ) as HTMLCanvasElement;
-    if (canvas.getContext) {
-      const ctx = canvas.getContext("2d");
-      game = new PongGame(
-        ctx,
-        socket,
-        setFps,
-        setSpeed,
-        setPlayer1Position,
-        setPlayer2Position,
-      );
-      game.draw_canvas();
-      setInterval(game.update, TARGET_FRAME_MS);
-    }
-  });
-
   return (
     <>
+      {/* <PongInformationBoard {} /> */}
       <div>
         <Button onClick={start}>Start</Button>
         <Button
           onClick={() => {
-            game.switch_battle_mode;
+            game.current.switch_battle_mode;
           }}
         >
           Battle
         </Button>
         <Button
           onClick={() => {
-            game.switch_practice_mode;
+            game.current.switch_practice_mode;
           }}
         >
           Practice
         </Button>
-        <div id="fps">FPS: {fps}</div>
-        <div id="speed">Speed: {speed}</div>
-        <div>
-          player1: <span id="player1">{player1Position}</span>
-        </div>
-        <div>
-          player2: <span id="player2">{player2Position}</span>
-        </div>
       </div>
-      <canvas id="tutorial" width="256" height="512"></canvas>
+      <canvas
+        ref={canvasRef}
+        width="256"
+        height="512"
+        className="border w-[256px] h-[512px]"
+      ></canvas>
     </>
   );
 }
