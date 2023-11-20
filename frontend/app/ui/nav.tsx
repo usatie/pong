d7a7b3026be1b3f1bc0297551cbe504899c9d97e
Shun Usami <usatie@gmail.com>
2023-11-20 17:12:33 +0900

    [frontend] Fix navbar

--- frontend/app/ui/nav.tsx
+++ frontend/app/ui/nav.tsx
@@ -1,37 +1,41 @@
-import Image from "next/image";
 import { ModeToggle } from "@/components/toggle-mode";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { signOut } from "@/app/lib/actions";
+import { isLoggedIn } from "@/app/lib/session";
 
-export default function Nav() {
+function AuthorizedMenu() {
+  return (
+    <li className="flex gap-8 items-center">
+      <Link href="/user">User List</Link>
+      <Link href="/room">ChatRoom List</Link>
+      <form action={signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+      <ModeToggle></ModeToggle>
+    </li>
+  );
+}
+
+function UnauhtorizedMenu() {
+  return (
+    <li className="flex gap-8 items-center">
+      <Link href="/user/signup">Sign Up</Link>
+      <Link href="/login">Log In</Link>
+      <ModeToggle></ModeToggle>
+    </li>
+  );
+}
+
+export default async function Nav() {
   return (
     <header>
       <nav>
         <ul className="flex items-center justify-between">
-          <li>
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={300 / 3}
-              height={68 / 3}
-              priority
-            />
-          </li>
-          <li className="flex gap-8 items-center">
-            <Link href="/">Home</Link>
-            <Link href="/user">User List</Link>
-            <Link href="/room">ChatRoom List</Link>
-            <Link href="/user/signup">Sign Up</Link>
-            <Link href="/playground/pong.html" target="_blank">
-              Pong
-            </Link>
-            <form action={signOut}>
-              <Button type="submit">Sign Out</Button>
-            </form>
-            <ModeToggle></ModeToggle>
-          </li>
+          <Link href="/" className="font-black">
+            Pong
+          </Link>
+          {(await isLoggedIn()) ? <AuthorizedMenu /> : <UnauhtorizedMenu />}
         </ul>
       </nav>
     </header>
