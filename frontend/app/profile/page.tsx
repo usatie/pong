b761dcbcea9849d5d545da4b686a391f2d96aca4
Shun Usami <usatie@users.noreply.github.com>
2023-11-20 23:30:11 +0900

    [WIP] Feat/frontend/profile (#67)
    * [frontend] Add skeleton with shadcn
    - `npx shadcn-ui@latest add skeleton`
    
    * [frontend] Add a separator with shadcn
    - `npx shadcn-ui@latest add separator`
    
    * [frontend] Add profile pages
    * [frontend] Fix navbar
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,48 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Separator } from "@/components/ui/separator";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Stack } from "@/app/ui/layout/stack";
+
+function AvatarSkeleton() {
+  return <Skeleton className="rounded-full h-20 w-20" />;
+}
+
+type ProfileItemProps = {
+  title: string;
+  value: string;
+};
+
+function ProfileItem({ title, value }: ProfileItemProps) {
+  return (
+    <Stack spacing={1} className="flex-initial w-96">
+      <div className="text-xs text-muted-foreground">{title}: </div>
+      <Input defaultValue={value} />
+    </Stack>
+  );
+}
+
+export default function ProfilePage() {
+  // Menu: min 100px
+  // Profile : the rest
+  return (
+    <>
+      <main>
+        <form>
+          <Stack spacing={4}>
+            <Stack spacing={1}>
+              <div className="text-2xl">Profile</div>
+              <Separator />
+            </Stack>
+            <AvatarSkeleton />
+            <ProfileItem title="username" value="susami" />
+            <ProfileItem title="email" value="susami@example.com" />
+            <Button variant="secondary" className="flex-initial w-40">
+              Save
+            </Button>
+          </Stack>
+        </form>
+      </main>
+    </>
+  );
+}
