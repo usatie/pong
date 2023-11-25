83049827afb867a14b5ad84975dcfffd6570f684
lim <lbooks424@gmail.com>
2023-11-25 14:12:32 +0900

    [frontend] Slightly improved sidebar ui

--- frontend/app/direct-message/dm-sidebar.tsx
+++ frontend/app/direct-message/dm-sidebar.tsx
@@ -1,3 +1,4 @@
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import { getUsers } from "@/app/lib/actions";
@@ -16,19 +17,27 @@ const DirectMessageSidebar = async () => {
     return null;
   }
   return (
-    <div className="flex flex-col h-full text-primary items-center w-full dark:bg-[#1E1F22] bg-[#F2F3F5]">
-      <h2>Other Users</h2>
-      <ScrollArea className="flex-1 px-3">
-        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
-        <div className="space-y-[2px]">
-          {users.map(
-            (user) =>
-              parseInt(currentUserId) !== user.id && (
-                <UserButton key={user.id} user={user} />
-              ),
-          )}
-        </div>
-      </ScrollArea>
+    <div>
+      <Card className="flex flex-col h-full text-primary w-full dark:bg-[#1E1F22] bg-[#F2F3F5]">
+        <CardHeader className="flex mt-1 py-1 items-center">
+          <b>Users</b>
+        </CardHeader>
+        <CardContent className="flex items-start p-3 pt-2">
+          <ScrollArea className="flex-1 px-1">
+            <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md mb-2" />
+            <div className="space-y-[2px]">
+                {users.map(
+                  (user) =>
+                    parseInt(currentUserId) !== user.id && (
+                    <ul key={user.id}>
+                      <UserButton key={user.id} user={user} />
+                    </ul>
+                    ),
+                )}
+            </div>
+          </ScrollArea>
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- frontend/app/direct-message/layout.tsx
+++ frontend/app/direct-message/layout.tsx
@@ -2,14 +2,14 @@ import DirectMessageSidebar from "@/app/direct-message/dm-sidebar";
 
 const DMlayout = async ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full">
+    <div className="flex h-full flex-row">
       <div
-        className="hidden md:flex h-full w-[100px]
-      z-20 flex-col fixed inset-y-100"
+        className="hidden md:flex h-full w-[140px]
+      z-20 flex-col inset-y-100"
       >
         <DirectMessageSidebar />
       </div>
-      <main className="md:pl-[100px] h-full">{children}</main>
+      <main className="md:pl-[140px] h-full">{children}</main>
     </div>
   );
 };
