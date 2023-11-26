af50e2880905e68925c7acd917d7f13cb9c39b6e
lim <lbooks424@gmail.com>
2023-11-26 14:20:21 +0900

    [add] Websocket with namespace and specific path

--- frontend/socket.ts
+++ frontend/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
-export const socket = io(`${process.env.NEXT_PUBLIC_WEB_URL}`, {
+export const socket = io(process.env.NEXT_PUBLIC_WEB_URL! + "/chat", {
+  path: "/socket.io/chat/",
   autoConnect: false,
 });
 
