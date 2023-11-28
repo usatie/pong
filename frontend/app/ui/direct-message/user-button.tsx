b201ceac0b7ea76073809caa2373c680b345c87b
lim <lbooks424@gmail.com>
2023-11-28 22:33:21 +0900

    [frontend] Context Menu added to direct message sidebar

--- frontend/app/ui/direct-message/user-button.tsx
+++ frontend/app/ui/direct-message/user-button.tsx
@@ -3,6 +3,13 @@
 import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import type { User } from "@/app/ui/user/card";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
 
 export const UserButton = ({ user }: { user: User }) => {
   const router = useRouter();
@@ -12,11 +19,23 @@ export const UserButton = ({ user }: { user: User }) => {
   };
 
   return (
-    <button
-      onClick={handleOnClick}
-      className="hover:text-black dark:hover:text-white text-slate-500 text-muted-foreground"
-    >
-      {user.name}
-    </button>
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <button
+          onClick={handleOnClick}
+          className="hover:text-black dark:hover:text-white text-slate-500 text-muted-foreground"
+        >
+          {user.name}
+        </button>
+      </ContextMenuTrigger>
+      <ContextMenuContent className="w-56">
+        <ContextMenuItem inset>Go profile</ContextMenuItem>
+        <ContextMenuSeparator />
+        <ContextMenuItem inset>Block</ContextMenuItem>
+        <ContextMenuItem inset disabled>
+          Unblock
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 };
