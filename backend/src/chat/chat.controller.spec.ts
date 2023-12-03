2cfbc9315ecea0a750ce3b3a1d8a5caf3d9221a1
lim <lbooks424@gmail.com>
2023-12-03 21:05:21 +0900

    [backend] Add PrismaService to test providers

--- backend/src/chat/chat.controller.spec.ts
+++ backend/src/chat/chat.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('ChatController', () => {
   let controller: ChatController;
@@ -8,7 +9,7 @@ describe('ChatController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChatController],
-      providers: [ChatService],
+      providers: [ChatService, PrismaService],
     }).compile();
 
     controller = module.get<ChatController>(ChatController);
--- backend/src/chat/chat.service.spec.ts
+++ backend/src/chat/chat.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ChatService } from './chat.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('ChatService', () => {
   let service: ChatService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatService],
+      providers: [ChatService, PrismaService],
     }).compile();
 
     service = module.get<ChatService>(ChatService);
