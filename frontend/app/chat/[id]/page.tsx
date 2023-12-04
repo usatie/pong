250711cfa9d71ac9c23428e51eee6d4d68cbe721
lim <lbooks424@gmail.com>
2023-12-04 22:34:50 +0900

    [frontend] Added fetch to page.tsx and created page per id

--- frontend/app/chat/[id]/page.tsx
+++ frontend/app/chat/[id]/page.tsx
@@ -1,15 +1,19 @@
 import { Separator } from "@/components/ui/separator";
-import { testData } from "./test-data";
-import { Sidebar } from "./sidebar";
-import MessageArea from "./message-area";
+import { testData } from "../test-data";
+import { Sidebar } from "../sidebar";
+import MessageArea from "../message-area";
 import { getUserId } from "@/app/lib/session";
-import { getUsers } from "@/app/lib/actions";
+import { getUser, getUsers } from "@/app/lib/actions";
 
 //async function getUsers() {
 //  return testData.users;
 //}
 
-export default async function ChatPage() {
+export default async function ChatPage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
   const [tmpUsers, currentUserId] = await Promise.all([
     getUsers(),
     getUserId(),
@@ -17,8 +21,15 @@ export default async function ChatPage() {
   if (!currentUserId) {
     throw new Error("getUserId error");
   }
+  const currentUser = await getUser(parseInt(currentUserId));
   const users = tmpUsers.filter((user) => user.id !== parseInt(currentUserId));
   if (!users) {
+    console.error("users error");
+    return null;
+  }
+  const otherUser = users.find((user) => String(user.id) === id);
+  if (!otherUser) {
+    console.error("other error");
     return null;
   }
   return (
@@ -26,6 +37,7 @@ export default async function ChatPage() {
       <div className="overflow-auto flex-grow flex gap-4 pb-4">
         <Sidebar users={users} />
         <Separator orientation="vertical" />
+        <MessageArea me={currentUser} other={otherUser} />
       </div>
     </>
   );
