4178507b7e3d29c6cd179a601d22cff2f2f5b75d
lim <lbooks424@gmail.com>
2023-12-04 22:36:32 +0900

    [frontend] Add logic to sidebar

--- frontend/app/chat/sidebar.tsx
+++ frontend/app/chat/sidebar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import { Stack } from "@/app/ui/layout/stack";
 import { SmallAvatarSkeleton } from "./skeleton";
-import type { User } from "./test-data";
+import type { User } from "@/app/ui/user/card";
+import { useRouter } from "next/navigation";
 
-function truncateString(str: string, num: number): string {
+function truncateString(str: string | undefined, num: number): string {
+  if (!str) {
+    return "";
+  }
   if (str.length <= num) {
     return str;
   }
@@ -11,12 +17,17 @@ function truncateString(str: string, num: number): string {
 
 export function Sidebar({ users }: { users: User[] }) {
   // TODO: If users is empty, show a message like "No users found"
+  const router = useRouter();
+  const handleOnClick = (user: User) => {
+    router.push(`/chat/${user.id}`);
+  };
   return (
     <div className="overflow-y-auto shrink-0 basis-36 pb-4">
       <Stack spacing={2}>
         {users.map((user) => (
           <button
             key={user.id}
+            onClick={() => handleOnClick(user)}
             className="flex gap-2 items-center group hover:opacity-60"
           >
             <SmallAvatarSkeleton />
