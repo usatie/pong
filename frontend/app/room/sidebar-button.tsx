a795e79bc9e65ed170dd9b3311862923dc0bd261
lim <lbooks424@gmail.com>
2023-12-12 00:34:55 +0900

    [frontend] Add socket event handlers for kick and updateRole

--- frontend/app/room/sidebar-button.tsx
+++ frontend/app/room/sidebar-button.tsx
@@ -11,11 +11,12 @@ import {
 import { Checkbox } from "@/components/ui/checkbox";
 import { Stack } from "@/app/ui/layout/stack";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { chatSocket as socket } from "@/socket";
 import type { User } from "@/app/ui/user/card";
-import { updateRoomUser, deleteRoomUser } from "@/app/lib/actions";
 import type { UserOnRoom, UserWithRole } from "./sidebar";
+import { updateRoomUser, deleteRoomUser } from "@/app/lib/actions";
+import { redirect, RedirectType } from "next/navigation";
 
 function truncateString(str: string | undefined, num: number): string {
   if (!str) {
@@ -42,8 +43,9 @@ export function SidebarButton({
   const [isChecked, setChecked] = useState(
     user.role === "OWNER" || user.role === "ADMINISTRATOR",
   );
-  const isMeOwner = myInfo.role === "OWNER";
-  const isMeAdmin = myInfo.role === "OWNER" || myInfo.role === "ADMINISTRATOR";
+  const [myRole, setMyRole] = useState(myInfo.role);
+  const isMeOwner = myRole === "OWNER";
+  const isMeAdmin = myRole === "OWNER" || myRole === "ADMINISTRATOR";
   const isOwner = user.role === "OWNER";
   let isAdmin = user.role === "OWNER" || user.role === "ADMINISTRATOR";
 
@@ -63,18 +65,48 @@ export function SidebarButton({
 
   const kickUser = (roomId: number, user: User) => {
     deleteRoomUser(roomId, user.id);
-    socket.emit("kick", user.id);
+    socket.emit("kick", { roomId, userId: user.id });
   };
 
   const handleCheckboxChange = (roomId: number, user: User) => {
     if (!isChecked) {
       updateRoomUser("ADMINISTRATOR", roomId, user.id);
+      socket.emit("updateRole", {
+        roomId,
+        userId: user.id,
+        role: "ADMINISTRATOR",
+      });
       setChecked(true);
     } else {
       updateRoomUser("MEMBER", roomId, user.id);
+      socket.emit("updateRole", { roomId, userId: user.id, role: "MEMBER" });
       setChecked(false);
     }
   };
+
+  useEffect(() => {
+    const handleKick = (kickId: number) => {
+      if (kickId === myInfo.userId) {
+        router.push("/room");
+      }
+    };
+
+    const handleUpdateRole = (role: string) => {
+      if (role === "MEMBER" || role === "ADMINISTRATOR") {
+        console.log("role", role);
+        setMyRole(role);
+      } else {
+        console.error("invalid role");
+      }
+    };
+    socket.on("kick", handleKick);
+    socket.on("updateRole", handleUpdateRole);
+
+    return () => {
+      socket.off("kick", handleKick);
+      socket.off("updateRole", handleUpdateRole);
+    };
+  }, [myInfo.userId, router]);
 
   return (
     <>
@@ -87,24 +119,33 @@ export function SidebarButton({
             <SmallAvatarSkeleton />
             <span className="text-muted-foreground text-sm whitespace-nowrap group-hover:text-primary">
               {truncateString(user.name, 15)}
+              {user.role === "OWNER" && "👑"}
+              {user.role === "ADMINISTRATOR" && "🛡"}
             </span>
           </button>
         </ContextMenuTrigger>
         <ContextMenuContent className="w-56">
           <ContextMenuItem inset>Go profile</ContextMenuItem>
           <ContextMenuSeparator />
-          <ContextMenuItem inset disabled={isBlocked}>
+          <ContextMenuItem
+            inset
+            disabled={isBlocked || myInfo.userId === user.id}
+          >
             <button onClick={() => blockUser(user)}>Block</button>
           </ContextMenuItem>
-          <ContextMenuItem inset disabled={!isBlocked}>
+          <ContextMenuItem
+            inset
+            disabled={!isBlocked || myInfo.userId === user.id}
+          >
             <button onClick={() => unblockUser(user)}>Unblock</button>
           </ContextMenuItem>
-          {isMeAdmin && myInfo.userId !== user.id && (
-            <ContextMenuItem inset disabled={isOwner}>
-              <button onClick={() => kickUser(roomId, user)}>Kick</button>
-            </ContextMenuItem>
-          )}
-          {isMeOwner && myInfo.userId !== user.id && (
+          {(myRole === "ADMINISTRATOR" || myRole === "OWNER") &&
+            myInfo.userId !== user.id && (
+              <ContextMenuItem inset disabled={isOwner}>
+                <button onClick={() => kickUser(roomId, user)}>Kick</button>
+              </ContextMenuItem>
+            )}
+          {myRole === "OWNER" && myInfo.userId !== user.id && (
             <ContextMenuItem inset disabled={isOwner}>
               <div className="flex items-center space-x-2">
                 <Checkbox
