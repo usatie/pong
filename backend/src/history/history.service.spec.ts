e54fd8b8f2a6ced110e9487796ba3ff5be9dd7f8
Shun Usami <usatie@gmail.com>
2023-12-12 11:43:40 +0900

    [backend] Remove history service spec

--- backend/src/history/history.service.spec.ts
+++ backend/src/history/history.service.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HistoryService } from './history.service';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { MatchResultType } from '@prisma/client';
 
 describe('HistoryService', () => {
   let service: HistoryService;
@@ -18,64 +17,5 @@ describe('HistoryService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-  });
-
-  describe('findAll', () => {
-    it('should return an array of matches with players and user details', async () => {
-      // Arrange
-      const userId = 1;
-      const expectedMatches = [
-        {
-          id: 1,
-          players: [
-            {
-              id: 1,
-              userId: 1,
-              user: {
-                id: 1,
-                name: 'John Doe',
-              },
-            },
-            {
-              id: 2,
-              userId: 2,
-              user: {
-                id: 2,
-                name: 'Jane Smith',
-              },
-            },
-          ],
-          result: 'COMPLETE' as MatchResultType,
-          createdAt: new Date(),
-        },
-        // Add more expected matches if needed
-      ];
-
-      jest
-        .spyOn(prismaService.match, 'findMany')
-        .mockResolvedValue(expectedMatches);
-
-      // Act
-      const result = await service.findAll(userId);
-
-      // Assert
-      expect(result).toEqual(expectedMatches);
-      expect(prismaService.match.findMany).toHaveBeenCalledWith({
-        where: {
-          players: {
-            some: {
-              userId,
-            },
-          },
-        },
-        include: {
-          players: {
-            include: {
-              user: true,
-            },
-          },
-        },
-      });
-    });
   });
 });
