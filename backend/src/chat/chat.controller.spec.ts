91ce98c21bdcc57ab77a0d9ddfe3e0eff549ffff
Shun Usami <usatie@gmail.com>
2023-12-15 17:29:03 +0900

    [backend] Fix unit tests for chat module

--- backend/src/chat/chat.controller.spec.ts
+++ backend/src/chat/chat.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('ChatController', () => {
   let controller: ChatController;
@@ -9,7 +11,7 @@ describe('ChatController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ChatController],
-      providers: [ChatService, PrismaService],
+      providers: [ChatService, PrismaService, AuthService, JwtService],
     }).compile();
 
     controller = module.get<ChatController>(ChatController);
