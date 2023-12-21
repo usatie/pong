da367910b17e5ae00329597c938e1e0dad9d9561
Shun Usami <usatie@gmail.com>
2023-12-22 00:03:41 +0900

    [backend] Fix friend request entity constructor

--- backend/src/friend-request/entities/friend-requests.entity.ts
+++ backend/src/friend-request/entities/friend-requests.entity.ts
@@ -3,12 +3,10 @@ import { PublicUserEntity } from 'src/user/entities/public-user.entity';
 
 export class FriendRequestsEntity {
   constructor(partial: Partial<FriendRequestsEntity>) {
-    this.requestedBy = partial.requestedBy.map(
-      (user) => new PublicUserEntity(user),
-    );
-    this.requesting = partial.requesting.map(
-      (user) => new PublicUserEntity(user),
-    );
+    this.requestedBy =
+      partial.requestedBy?.map((user) => new PublicUserEntity(user)) ?? [];
+    this.requesting =
+      partial.requesting?.map((user) => new PublicUserEntity(user)) ?? [];
   }
 
   @ApiProperty()
