f7b78e2219e56fb74b3bb8999f57c3fdc1478d1f
Shun Usami <usatie@gmail.com>
2023-12-22 15:19:45 +0900

    [frontend] Add tooltip to friends list

--- frontend/app/ui/user/friends.tsx
+++ frontend/app/ui/user/friends.tsx
@@ -1,4 +1,10 @@
 import { getFriends } from "@/app/lib/actions";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import Link from "next/link";
 import { Avatar } from "./avatar";
 import ProfileItem from "./profile-item";
@@ -7,18 +13,25 @@ export default async function Friends({ userId }: { userId: number }) {
   const friends = await getFriends(userId);
   return (
     <ProfileItem title="Friends">
-      <div className="flex gap-1">
-        {friends.length === 0 && <div>No friends</div>}
-        {friends.map((friend) => (
-          <Link href={`/user/${friend.id}`} key={friend.id}>
-            <Avatar
-              avatarURL={friend.avatarURL}
-              size="medium"
-              alt={friend.name}
-            />
-          </Link>
-        ))}
-      </div>
+      <TooltipProvider delayDuration={0}>
+        <div className="flex gap-1">
+          {friends.length === 0 && <div>No friends</div>}
+          {friends.map((friend) => (
+            <Tooltip key={friend.id}>
+              <TooltipTrigger>
+                <Link href={`/user/${friend.id}`} key={friend.id}>
+                  <Avatar
+                    avatarURL={friend.avatarURL}
+                    size="medium"
+                    alt={friend.name}
+                  />
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent>{friend.name}</TooltipContent>
+            </Tooltip>
+          ))}
+        </div>
+      </TooltipProvider>
     </ProfileItem>
   );
 }
