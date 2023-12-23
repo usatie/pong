d861373fc88657c3d6ade6d6ae96d6dd8ceb0660
Shun Usami <usatie@users.noreply.github.com>
2023-12-23 14:33:08 +0900

    ユーザ一覧コンポーネントを作成 (#171)
    * [frontend] Add user list component
    * [backend] Add friends to seed script
    * [frontend] Remove user card component

---
+++ frontend/app/ui/user/user-list.tsx
@@ -0,0 +1,39 @@
+import { PublicUserEntity } from "@/app/lib/actions";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import Link from "next/link";
+import { Avatar, AvatarSize } from "./avatar";
+
+export default function UserList({
+  users,
+  avatarSize,
+}: {
+  users: PublicUserEntity[];
+  avatarSize: AvatarSize;
+}) {
+  return (
+    <TooltipProvider delayDuration={0}>
+      <div className="flex flex-wrap gap-2">
+        {users.length === 0 && <div>No users to display</div>}
+        {users.map((u) => (
+          <Tooltip key={u.id}>
+            <TooltipTrigger>
+              <Link href={`/user/${u.id}`} key={u.id}>
+                <Avatar
+                  avatarURL={u.avatarURL}
+                  size={avatarSize}
+                  alt={u.name}
+                />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>{u.name}</TooltipContent>
+          </Tooltip>
+        ))}
+      </div>
+    </TooltipProvider>
+  );
+}
