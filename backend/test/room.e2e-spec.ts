a271aa81eb2e54a475759376fb0aeba8a33238a9
Shun Usami <usatie@gmail.com>
2023-12-23 21:39:38 +0900

    [backend] Add e2e test for create various rooms

--- backend/test/room.e2e-spec.ts
+++ backend/test/room.e2e-spec.ts
@@ -80,27 +80,55 @@ describe('RoomController (e2e)', () => {
   });
 
   describe('POST /room (Create Room)', () => {
-    let testRoom: RoomEntity;
+    let _publicRoom: RoomEntity;
+    let _privateRoom: RoomEntity;
+    let _protectedRoom: RoomEntity;
+    let _duplicatedRoom: RoomEntity;
 
     afterAll(async () => {
-      await app.deleteRoom(testRoom.id, owner.accessToken);
+      await app.deleteRoom(_publicRoom.id, owner.accessToken);
+      await app.deleteRoom(_privateRoom.id, owner.accessToken);
+      await app.deleteRoom(_protectedRoom.id, owner.accessToken);
+      await app.deleteRoom(_duplicatedRoom.id, owner.accessToken);
     });
 
     it('should create public room (201 Created)', async () => {
-      testRoom = await app
+      _publicRoom = await app
         .createRoom(constants.room.publicRoom, owner.accessToken)
         .expect(201)
         .expect((res) => expectRoom(res.body))
         .then((res) => res.body);
     });
 
-    it('should create private room (201 Created)', async () => {});
+    it('should create private room (201 Created)', async () => {
+      _privateRoom = await app
+        .createRoom(constants.room.privateRoom, owner.accessToken)
+        .expect(201)
+        .expect((res) => expectRoom(res.body))
+        .then((res) => res.body);
+    });
 
-    it('should create password protected room (201 Created)', async () => {});
+    it('should create password protected room (201 Created)', async () => {
+      _protectedRoom = await app
+        .createRoom(constants.room.protectedRoom, owner.accessToken)
+        .expect(201)
+        .expect((res) => expectRoom(res.body))
+        .then((res) => res.body);
+    });
 
-    it('should not create protected room without password (400 Bad Request)', async () => {});
+    it('should not create protected room without password (400 Bad Request)', async () => {
+      let dtoWithoutPassword = { ...constants.room.protectedRoom };
+      delete dtoWithoutPassword.password;
+      await app.createRoom(dtoWithoutPassword, owner.accessToken).expect(400);
+    });
 
-    it('should create room with duplicated name (201 OK)', async () => {});
+    it('should create room with duplicated name (201 OK)', async () => {
+      _duplicatedRoom = await app
+        .createRoom(constants.room.publicRoom, owner.accessToken)
+        .expect(201)
+        .expect((res) => expectRoom(res.body))
+        .then((res) => res.body);
+    });
   });
 
   describe('GET /room/:id (Get Room)', () => {
