dba723e2fe5b3dec80038e95c99e8d790e038f18
Takumi Hara <takumihara1226@gmail.com>
2023-12-25 02:23:52 -0800

    Implement authentication for gateway

--- backend/src/events/events.module.ts
+++ backend/src/events/events.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
 import { PongMatchGateway } from './pong-match.gateway';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   providers: [EventsGateway, PongMatchGateway],
+  imports: [AuthModule],
 })
 export class EventsModule {}
