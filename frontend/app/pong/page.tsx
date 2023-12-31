c6da54c767a5ea39c7c414b3a0e00a019d3579df
Takumi Hara <takumihara1226@gmail.com>
2023-12-30 23:57:58 -0800

    Fix format of game top page

--- frontend/app/pong/page.tsx
+++ frontend/app/pong/page.tsx
@@ -10,12 +10,9 @@ export default async function Page() {
   const isAuthorized = await isLoggedIn();
   const roomId = v4();
   return (
-    <div className="flex flex-col content-between gap-5 mx-auto">
-      {/* todo: use asChild */}
-      <Button className="inline max-w-sm" disabled={!isAuthorized}>
-        <Link href={`/pong/${roomId}`} className="block">
-          Create a new room
-        </Link>
+    <div className="flex flex-col gap-5 items-center">
+      <Button disabled={!isAuthorized} asChild>
+        <Link href={`/pong/${roomId}`}>Create a new room</Link>
       </Button>
       <MatchButton disabled={!isAuthorized}></MatchButton>
       <JoinRoomForm disabled={!isAuthorized} />
