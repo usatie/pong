013eb1c012051809ce6405d9a7cd47dc0c6d7f6a
lim <lbooks424@gmail.com>
2024-01-09 17:15:47 +0900

    [frontend] Fixed to keep block state

--- frontend/app/room/[id]/sidebar-item.tsx
+++ frontend/app/room/[id]/sidebar-item.tsx
@@ -2,11 +2,12 @@
 
 import {
   blockUser,
+  getBlockingUsers,
   kickUserOnRoom,
   unblockUser,
   updateRoomUser,
 } from "@/app/lib/actions";
-import type { UserOnRoomEntity } from "@/app/lib/dtos";
+import type { PublicUserEntity, UserOnRoomEntity } from "@/app/lib/dtos";
 import { SmallAvatarSkeleton } from "@/app/ui/room/skeleton";
 import {
   ContextMenu,
@@ -16,6 +17,7 @@ import {
   ContextMenuTrigger,
 } from "@/components/ui/context-menu";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 function truncateString(str: string | undefined, num: number): string {
   if (!str) {
@@ -43,10 +45,21 @@ export default function SidebarItem({
   user: UserOnRoomEntity;
   me: UserOnRoomEntity;
 }) {
+  const [isBlocked, setIsBlocked] = useState(false);
   const isUserAdmin = user.role === "ADMINISTRATOR";
   const isUserOwner = user.role === "OWNER";
   const isMeAdminOrOwner = me.role === "ADMINISTRATOR" || me.role === "OWNER";
-  const isBlocked = false; // TODO: user.blockedBy.contains((u) => u.id === me.userId);
+
+  useEffect(() => {
+    const fetchGetBlockingUsers = async () => {
+      const blockingUsers = await getBlockingUsers();
+      const isBlocked = blockingUsers.some(
+        (u: PublicUserEntity) => u.id === user.userId,
+      );
+      setIsBlocked(isBlocked);
+    };
+    fetchGetBlockingUsers();
+  }, []);
   const router = useRouter();
   const openProfile = () => {
     if (user.userId === me.userId) {
@@ -55,8 +68,14 @@ export default function SidebarItem({
       router.push(`/user/${user.userId}`);
     }
   };
-  const block = () => blockUser(user.userId);
-  const unblock = () => unblockUser(user.userId);
+  const block = () => {
+    blockUser(user.userId);
+    setIsBlocked(true);
+  };
+  const unblock = () => {
+    unblockUser(user.userId);
+    setIsBlocked(false);
+  };
   const kick = () => kickUserOnRoom(roomId, user.userId);
   const updateUserRole = isUserAdmin
     ? () => updateRoomUser("MEMBER", roomId, user.userId)
