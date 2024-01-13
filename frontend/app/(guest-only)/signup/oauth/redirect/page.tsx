df936bece626795b2a968cf8e1a7362f415c3ea1
dorakueYaritai <kotto555555@gmail.com>
2024-01-13 23:20:45 +0900

    /oauth/signup/oauth/redirect this page redirect by authorization server to client. this page request to authorization server to get accessToken. TODO: signup に必要な情報を取り出してsignup に使う accessToken refreshToken もdb に保存する?

---
+++ frontend/app/(guest-only)/signup/oauth/redirect/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+const redirect_page = async () => {
+  const params = useSearchParams();
+  if (params.has("error")) {
+    const reason = params.get("error");
+    return <>Error です {reason}</>;
+  } else {
+    // TODO state の確認(SHOULD)
+
+    // TODO 認可コードの取得
+    const code = params.get("code");
+    console.log(code);
+
+    const client_secret = process.env.OAUTH_GOOGLE_CLIENT_SECRET;
+    const client_id = process.env.OAUTH_GOOGLE_CLIENT_ID;
+
+    const formData =
+      `code=${code}` +
+      `&client_id=${client_id}` +
+      `&client_secret=${client_secret}` +
+      `&redirect_uri=http://localhost:4242/signup/oauth/redirect` +
+      `&grant_type=authorization_code`;
+
+    const headers = {
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+    const tokRes: Response = await fetch(
+      //   "http://localhost:80",
+      "https://accounts.google.com/o/oauth2/token",
+      {
+        method: "POST",
+        headers: headers,
+        body: formData,
+      }
+    );
+
+    // // TODO accesstoken とる
+    if (tokRes.status >= 200 && tokRes.status < 300) {
+      const body = tokRes.body;
+      console.log(tokRes);
+      console.log(body);
+    } else console.log(tokRes.status);
+    // TODO 保存する
+    // TODO resource とる
+    return <>{code}</>;
+  }
+};
+
+export default redirect_page;
