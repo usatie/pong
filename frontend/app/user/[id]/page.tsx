e9e49d517506052897da16ba2c849be013d898c2
lim396 <90199432+lim396@users.noreply.github.com>
2024-01-13 14:37:15 +0900

    [frontend] Can be blocked from user profiles (#205)

--- frontend/app/user/[id]/page.tsx
+++ frontend/app/user/[id]/page.tsx
@@ -1,7 +1,13 @@
-import { getFriendRequests, getFriends, getUser } from "@/app/lib/actions";
+import {
+  getBlockingUsers,
+  getFriendRequests,
+  getFriends,
+  getUser,
+} from "@/app/lib/actions";
 import { getCurrentUserId } from "@/app/lib/session";
 import AcceptFriendButton from "@/app/ui/user/accept-friend-request-button";
 import AddFriendButton from "@/app/ui/user/add-friend-button";
+import BlockButton from "@/app/ui/user/block-button";
 import { Avatar } from "@/app/ui/user/avatar";
 import CancelFriendRequestButton from "@/app/ui/user/cancel-friend-request-button";
 import Friends from "@/app/ui/user/friends";
@@ -10,6 +16,7 @@ import MatchRequestButton from "@/app/ui/user/match-request-button";
 import RejectFriendButton from "@/app/ui/user/reject-friend-request-button";
 import RemoveFriendButton from "@/app/ui/user/remove-friend-button";
 import Stats from "@/app/ui/user/stats";
+import UnBlockButton from "@/app/ui/user/unblock-button";
 
 export default async function FindUser({
   params: { id },
@@ -21,11 +28,13 @@ export default async function FindUser({
   const requests = await getFriendRequests();
   const currentUserId = await getCurrentUserId();
   const myFriends = await getFriends(currentUserId);
+  const blockingUsers = await getBlockingUsers();
   // check if any requesting contains userId
   const hasSentRequest = requests.requesting.some((r) => r.id === userId);
   const hasReceivedRequest = requests.requestedBy.some((r) => r.id === userId);
   const isFriend = myFriends.some((f) => f.id == userId);
   const canAddFriend = !hasSentRequest && !hasReceivedRequest && !isFriend;
+  const isBlocking = blockingUsers.some((b) => b.id === userId);
   // TODO: Must consider these situations
   // 1. Already friends
   // 2. Friend request sent
@@ -48,6 +57,8 @@ export default async function FindUser({
             {hasReceivedRequest && <RejectFriendButton id={userId} />}
             {isFriend && <RemoveFriendButton id={userId} />}
             {canAddFriend && <AddFriendButton id={userId} />}
+            {!isBlocking && <BlockButton id={userId} />}
+            {isBlocking && <UnBlockButton id={userId} />}
           </div>
           <MatchRequestButton id={userId} />
         </>
