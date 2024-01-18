efc6ec64b06ac46c8985254126d5a1f541de2d4a
kotto5 <kotto555555@gmail.com>
2024-01-18 23:00:39 +0900

    [frontend] Add login with 42 option to login page

--- frontend/app/(guest-only)/login/page.tsx
+++ frontend/app/(guest-only)/login/page.tsx
@@ -1,5 +1,10 @@
 import LoginForm from "@/app/ui/auth/login-form";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <>
+      <LoginForm />
+      <a href="http://localhost:4242/api/auth/login/oauth2/42">login with 42</a>
+    </>
+  );
 }
