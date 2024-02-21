3cfc4e292b9d56db6c2c27ccc860b4f09d6a19d5
Takumi Hara <69781798+takumihara@users.noreply.github.com>
2024-02-21 00:31:45 -0600

    Remove join room button (#275)

--- frontend/app/pong/JoinRoomForm.tsx
+++ frontend/app/pong/JoinRoomForm.tsx
@@ -8,12 +8,8 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 import { v4 } from "uuid";
 import MatchButton from "./MatchButton";
 
@@ -21,15 +17,11 @@ export default function JoinRoomForm({}) {
   return (
     <Card className="basis-1/3">
       <CardHeader>
-        <CardTitle>Join room</CardTitle>
-        <CardDescription>
-          Join a room to play pong with your friend
-        </CardDescription>
+        <CardTitle>Play Pong</CardTitle>
+        <CardDescription>Lets play pong with your friend</CardDescription>
       </CardHeader>
       <CardContent>
         <div className="flex flex-col gap-4">
-          <Form />
-          <OrSeparator />
           <CreateRoomButton />
           <OrSeparator />
           <MatchButton />
@@ -55,29 +47,5 @@ function CreateRoomButton() {
     <Button asChild>
       <Link href={`/pong/${randomRoomId}`}>Create a new room</Link>
     </Button>
-  );
-}
-
-function Form() {
-  const router = useRouter();
-  const [inputValue, setInputValue] = useState("");
-  const handleSubmit = (event: React.SyntheticEvent) => {
-    event.preventDefault();
-    router.push(`/pong/${inputValue}?mode=player`);
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="grid w-full items-center gap-4">
-        <div className="flex flex-col space-y-1.5">
-          <Label htmlFor="id">ID</Label>
-          <Input
-            id="id"
-            placeholder="ID of pong room you friend told you"
-            onChange={(e) => setInputValue(e.target.value)}
-          />
-        </div>
-        <Button>Join</Button>
-      </div>
-    </form>
   );
 }
