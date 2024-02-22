ba20a44459c77ae7fa12ec17a80ffff8520956cb
Takumi Hara <takumihara1226@gmail.com>
2024-02-22 00:47:01 -0600

    Handle undefined player in GameCard

--- frontend/app/ui/pong/GameCard.tsx
+++ frontend/app/ui/pong/GameCard.tsx
@@ -6,17 +6,21 @@ import { Card } from "@/components/ui/card";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { PublicUserEntity } from "../../lib/dtos";
 
-function UserCard({ user }: { user: PublicUserEntity }) {
+function UserCard({ user }: { user?: PublicUserEntity }) {
+  const name = user?.name || "";
+  const avatarUrl = user?.avatarURL || "/avatar/undefined.jpg";
+  const href = user ? `/user/${user.id}` : undefined;
+  const id = user?.id;
   return (
     <>
       <div className="flex gap-4 items-center">
-        {user.name}
+        {name}
         <Avatar
-          avatarURL={user.avatarURL}
+          avatarURL={avatarUrl}
           size="medium"
-          href={`/user/${user.id}`}
-          alt={user.name}
-          id={user.id}
+          href={href}
+          alt={name}
+          id={id}
         />
       </div>
     </>
@@ -27,8 +31,8 @@ export function GameCard({
   leftPlayer,
   rightPlayer,
 }: {
-  leftPlayer: PublicUserEntity;
-  rightPlayer: PublicUserEntity;
+  leftPlayer?: PublicUserEntity;
+  rightPlayer?: PublicUserEntity;
 }) {
   return (
     <Card className="w-[350px] p-4 flex flex-col gap-2 items-center">
