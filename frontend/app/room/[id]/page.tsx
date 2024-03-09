5a77abc4cfd13cabad5e8cdc1fee22ade6d85987
lim <lbooks424@gmail.com>
2024-03-09 16:11:44 +0900

    [frontend] Fixed to call notFound function instead of throwing Error object

--- frontend/app/room/[id]/page.tsx
+++ frontend/app/room/[id]/page.tsx
@@ -3,6 +3,7 @@ import { Separator } from "@/components/ui/separator";
 import { notFound } from "next/navigation";
 import MessageArea from "./message-area";
 import RoomDetail from "./room-detail";
+import { getCurrentUserId } from "@/app/lib/session";
 
 export default async function Page({
   params: { id },
@@ -18,12 +19,22 @@ export default async function Page({
   }
   const messages = await getMessages(roomId);
   const allUsers = await getUsers();
+  const currentUserId = await getCurrentUserId();
+  const me = room.users.find((u) => u.userId === currentUserId);
+  if (!me) {
+    notFound();
+  }
   return (
     <>
       <div className="overflow-auto flex-grow flex gap-4 pb-4">
         <MessageArea roomId={roomId} messages={messages} />
         <Separator orientation="vertical" />
-        <RoomDetail room={room} users={room.users} allUsers={allUsers} />
+        <RoomDetail
+          room={room}
+          users={room.users}
+          allUsers={allUsers}
+          me={me}
+        />
       </div>
     </>
   );
